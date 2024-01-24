3ff30dcc89881d67596e0e070336f149b521e6ec
52nt14/nand <95685250+52nt14@users.noreply.github.com>
2024-01-24 02:54:51 +0000

    update

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import rsa
-(pub_key, pri_key) = rsa.newkeys(2048)
-text = "Distroted_Fate".encode("utf8")
-crypto = rsa.encrypt(text,pub_key)
-decode = rsa.decrypt(crypto, pri_key)
-print(pub_key.n)
-print("\n")
-print(pub_key.e)
-print("\n")
-print("\n")
-print("\n")
-print(pri_key.d)
-print("\n")
-print(pri_key.e)
-print("\n")
-print(pri_key.n)
-print("\n")
-print(pri_key.p)
-print("\n")
-print(pri_key.q)
-print("\n")
-print("\n")
-print("\n")
-print(dir(crypto))
-print("\n")
-print(decode.decode("utf8"))
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+def rsa_enc(x,y):
+    z = rsa.encrypt(x,y)
+    return z
+def aes_enc(x,y):
+    x_byte = x.encode("utf-8")
+    z = get_random_bytes(12)
+    z = AES.new(y, AES.MODE_GCM, nonce=z)
+    z, tag = z.encrypt_and_digest(x_byte)
+    return z,tag
+string = input("暗号化する文章を入力===>")
+(pub_key, pri_key) = rsa.newkeys(4096)
+p = pri_key.p
+q = pri_key.q
+d = pri_key.d
+e = pub_key.e
+n = pub_key.n
+string_enc = rsa_enc(string,pub_key)
+string_enc , tag = aes_enc(string_enc,n)
+print("暗号化文" + string_enc)
+print("TAG" + tag)
